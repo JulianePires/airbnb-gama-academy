501a992d5b847cc873c9bca2a755060b27fbb11c
Juliane Pires <mpiresjuliane@outlook.com>
2020-07-11 17:15:03 -0300

    commit inicial

--- src/main.js
+++ src/main.js
@@ -3,53 +3,43 @@ const API_URL = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 // Para isso, usaremos o "fetch", conforme você deve ter visto nas aulas online.
 // Corrija a linha abaixo, para que a requisição funcione.
-XMLHttpRequest(API_URI)
-  .then(response => response.text())
+fetch(API_URI)
+  .then(response => response.json())
   .then(result => {
-    // Aqui, estamos pegando o resultado da requisição e parseando em JSON, 
-    // para que fique mais fácil e amigável, tratarmos esses dados.
-    const data = JSON.parse(result);
-
-    // Como é mesmo que percorremos um array? 'hm
-    dados.map(element => {
+    const row = document.getElementById("row");
+  
+    result.forEach(item => {
       // Desestruture seu objeto, trazendo os seguintes dados:
       // Foto, tipo, nome e preço da propriedade.
-      const { variavel, chave } = data;
+      const { photo, property_Type, property_Name, property_Price } = item;
 
-      // Pegue a linha do container onde serão exibidos os cards.
-      row = document.pega("linha");
+      let div = document.createElement("div");
+      div.className = "col-md-4 altura";
 
-      // Agora, crie um elemento "div" com a class "col-md-4" do Bootstrap.
-      div = document.criaElemento("div");
-      div.nomeDaClasse = "col-md-4";
+      let card = document.createElement("div");
+      card.className = "card mb-4 box-shadow";
 
-      // Boa! Agora, precisamos criar a "div" do card a ser exibido. Ao criar, atribua a class "card mb-4 box-shadow".
-      card = ?
+      let image = document.createElement("img");
+      div.classList.add("card-img-top");
+      image.src = photo;
 
-      // Humm... Agora que já temos o card, que tal começarmos a popular as informações?
-      // Crie um elemento "img" e, atribua a class como "card-img-top";
-      // Não se esqueça que sua imagem precisa ser referenciada!
-      image.className = "card-img-top";
-      image.referencia = minhaImagem;
+      let cardBody = document.createElement("div");
+      div.classList.add("card-body");
 
-      // Legal! Agora, já podemos começar a construir o "corpo" do card. Bora lá!
-      // Obs.: atribua a class "card-body" do Bootstrap, para que fique bonitão. :)
-      cardBody = ?
+      let cardText = document.createElement("div");
+      div.classList.add("card-text");
 
-      // Beleza, corpo criado. Seguindo nossa estrutura, o próximo elemento seria os textos, para exibir
-      // o tipo da propriedade, nome e preço. Mas, antes, vamos criar um elemento "div" para manter isso
-      // tudo organizado. Você pode atribuir a class "card-text" do Bootstrap!
-      cardText = ?
-
-      // Uffa! Finalmente os elementos da proriedade!
-      // Precisamos agora, listar o tipo, o nome e o preço.
-      propertyType = criaParagrafo("p");
+      let propertyType = createElement("p");
       propertyType.className = "property-type";
-      propertyType.innerHTML = element;
+      propertyType.innerHTML = property_Type;
 
-      propertyName = ?
+      let propertyName = createElement("p");
+      propertyName.className = "property-name";
+      propertyName.innerHTML = property_Name;
 
-      propertyPrice = ?
+      propertyPrice = createElement("p");
+      propertyPrice.className = "property-price";
+      propertyPrice.innerHTML = property_Price;
 
       // AOBA! Temos agora, todos os elementos necessários para percorrer a resposta da API e,
       // anexar um ao outro para exibição ao usuário. Para "anexar" os elementos, nós usaremos
@@ -67,6 +57,13 @@ XMLHttpRequest(API_URI)
       //           propertyPrice
 
       row.appendChild(div);
+      div.appendChild(card);
+      card.appendChild(image);
+      card.appendChild(cardBody);
+      cardBody.appendChild(cardText);
+      cardText.appendChild(propertyType);
+      cardText.appendChild(propertyName);
+      cardText.appendChild(propertyPrice);
       // [ ... ]
-    });
-  });
+    })
+  })
